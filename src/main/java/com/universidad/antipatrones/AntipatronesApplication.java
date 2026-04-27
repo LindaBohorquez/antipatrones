4bbb135173d007cafbaca28ff00d1b16b3210f34
Linda Bohorquez <96446503+LindaBohorquez@users.noreply.github.com>
2026-04-27 04:27:10 -0500

    Primer commit

---
+++ src/main/java/com/universidad/antipatrones/AntipatronesApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.antipatrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntipatronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntipatronesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universidad/antipatrones/AntipatronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.antipatrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntipatronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
